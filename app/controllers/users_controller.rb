d3c0ec662971215b8aeaf8621fddefa751d4e136
Naihebaver <vladyslav.naihebaver@gmail.com>
2017-10-21 23:03:01 +0300

    add country, city, house number

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,7 +39,7 @@ class UsersController < ApplicationController
   private 
 
   def user_params
-  	params.require(:user).permit(:lastname, :firstname,:email,:password)
+  	params.require(:user).permit(:lastname, :firstname,:email,:password, :country, :city, :house_number)
   end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171009092302) do
+ActiveRecord::Schema.define(version: 20171021194402) do
 
   create_table "users", force: :cascade do |t|
     t.string "firstname"
@@ -19,6 +19,9 @@ ActiveRecord::Schema.define(version: 20171009092302) do
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "country"
+    t.string "city"
+    t.integer "house_number"
   end
 
 end
