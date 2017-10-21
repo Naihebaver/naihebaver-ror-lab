4182aef4c558d8152bf7150ee79c0cb25e26ddce
Naihebaver <vladyslav.naihebaver@gmail.com>
2017-10-21 23:12:41 +0300

    add validation to email, password and house number

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
 	validates :firstname, :lastname, :email, :password, presence: true
+	validates :email, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+	validates :password, length: { in: 3..6 }
+	validates :house_number, numericality: { only_integer: true }
 end
